fe27ed4d2de7ad4f25b4a8966869db187cd40670
Girindra Syukran Prahasto <girindra220202@gmail.com>
2024-12-09 11:57:06 +0700

    feat: add new nav items component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import walledlogo from "./assets/Vector.png"
+import NavItems from "./components/NavItems";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const menu = [
+    {
+      title: "Dashboard",
+      link: "#"
+    },
+    {
+      title: "Transfer",
+      link: "#"
+    },
+    {
+      title: "Top Up",
+      link: "#"
+    },
+    {
+      title: "Sign Out",
+      link: "#"
+    }
+  ]
 
   return (
     <>
-      <p>Walled</p>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      <nav className = "flex items-center w-screen justify-between bg-white py-4 px-8">
+        <img src = {walledlogo}/>
+        <div className="flex gap-x-8">
+          <NavItems menu={menu} />
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavItems.jsx
@@ -0,0 +1,11 @@
+function NavItems({ menu }) {
+    return (
+      <div className="flex gap-x-8">
+        {menu.map((item, index) => (
+          <a key={index} href={item.link}>{item.title}</a>
+        ))}
+      </div>
+    );
+  }
+  
+  export default NavItems;
